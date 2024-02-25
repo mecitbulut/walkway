aafb9cdd51f4236e92d5a7bd273526f5bb5df698
mecit bulut <mecitbulut@gmail.com>
2024-02-25 21:39:49 +0300

    some changes been updated

--- walkway.js
+++ walkway.js
@@ -10,36 +10,24 @@ class World {
     constructor(location, startLocation) {
         this.locations = location;
 
-        console.log("You are at", startLocation)
-        this.locations = this.locations;
-        this.currLocation = startLocation;
+        console.log("You are at", startLocation);
+        this.currtLocation = this.locations[startLocation];
     }
 
-    connectedLocations(loc, connections) {
+    connectLocations(loc, connections) {
         connections.forEach((conn) => {
-            //  this.locations[loc].connectedLocations.push(this.locations[conn]);
-            //  this.locations[conn].connectedLocations.push(this.locations[loc]);
 
-            this.locations[loc].connectLocations[conn] = this.connectLocations[conn];
-            this.locations[loc].connectLocations[loc] = this.connectLocations[loc];
+            this.locations[loc].connectedLocations[conn] = this.locations[conn];
+            this.locations[loc].connectedLocations[loc] = this.locations[loc];
          });
     }
 
     move(newLocation) {
-        // this.currLocation.connectedLocations.forEach((loc) => {
-            // if (loc.name === newLocation) {
-                // console.log("You move to", newLocation)
-                // this.currLocation = this.locations[newLocation];
-                // return;
-            // }
-        // });
-
-
-        if (newLocation in this.currentLocation.connectLocations) {
+        if (newLocation in this.currLocation.connectedLocations) {
             console.log("You move to", newLocation);
-            this.currentLocation = this.locations[newLocation];
+            this.currLocation = this.locations[newLocation];
         } else {
-            console.log("You can't go from", this.currentLocation.name, "to", newLocation);
+            console.log("You can't go from", this.currLocation.name, "to", newLocation);
         }
     }
 }
@@ -48,15 +36,17 @@ let world = new World ({
     home: new Location("home"),
     store: new Location("store"),
     friendsHouse: new Location("friendHouse"),
-    sidewalk: new Location("sidewalk")
-}, "home",
+    sidewalk: new Location("sidewalk"),
+    }, "home");
 
 
-world.connectedLocations("sidewalk", ["home", "store", "friendHouse"]);
+
+world.connectLocations("sidewalk", ["home", "store", "friendHouse"]);
 world.connectLocations("home", ["friendHouse"]);
 
-console.log(world.locations.home.connectLocations)
 world.move("sidewalk");
 world.move("store");
 world.move("home");
 world.move("friendHouse");
+world.move("sidewalk");
+world.move("home");
